a280e6f51dad5f1e5f1a663e9b8e73e04cf887ca
Lynn <62611426+Lynn-Dev@users.noreply.github.com>
2023-08-01 16:07:31 +1000

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3603,14 +3603,14 @@ function Library:CreateWindow(...)
     end
 
     Library:GiveSignal(InputService.InputBegan:Connect(function(Input, Processed)
-        if type(Library.ToggleKeybind) == 'table' and Library.ToggleKeybind.Type == 'KeyPicker' then
-            if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode.Name == Library.ToggleKeybind.Value then
-                task.spawn(Library.Toggle)
-            end
-        elseif Input.KeyCode == Enum.KeyCode.RightControl or (Input.KeyCode == Enum.KeyCode.RightShift and (not Processed)) then
+    if type(Library.ToggleKeybind) == 'table' and Library.ToggleKeybind.Type == 'KeyPicker' then
+        if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode.Name == Library.ToggleKeybind.Value then
             task.spawn(Library.Toggle)
         end
-    end))
+    elseif Input.KeyCode == Enum.KeyCode.Delete then -- Change RightControl to Delete
+        task.spawn(Library.Toggle)
+    end
+end))
 
     if Config.AutoShow then task.spawn(Library.Toggle) end
 
